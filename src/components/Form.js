33dd375a48fd90070c31896e8d2f21896e85dd34
dragosbus <dragosbusuioc94@gmail.com>
2018-05-11 20:38:00 +0200

    the form is styled

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import FaSearch from 'react-icons/lib/fa/search';
 
 
 export class Form extends Component {
     render() {
         return (
-            <form>
+            <form id="movie-search">
                 <input type="search" />
-                <button type="submit">S</button>
+                <button type="submit">
+                    <FaSearch/>
+                </button>
             </form>
         );
     }
