625ef344ac3ecb8a9c4bd3efd2f3882265f8d66c
dragosbus <dragosbusuioc94@gmail.com>
2018-05-11 20:24:15 +0200

    created form component

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export class Form extends Component {
+    render() {
+        return (
+            <form>
+                <input type="search" />
+                <button type="submit">S</button>
+            </form>
+        );
+    }
+}
