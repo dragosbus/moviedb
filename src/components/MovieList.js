c080ee471ae83c99124a5996a431784339428992
dragosbus <dragosbusuioc94@gmail.com>
2018-05-13 17:49:15 +0200

    created circle with rating

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,6 +6,9 @@ export const Movie = props => {
         <li className="movie">
             <img src={`https://image.tmdb.org/t/p/w500${props.cover}`} alt="Cover" />
             <div className="infos">
+                <div className="rating">
+                    <span className="score">{props.rating * 10}%</span>
+                </div>    
                 <h3 className="title">{props.title}</h3>
             </div>
         </li>
