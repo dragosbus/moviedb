6edb6984796ce0fba459061d8fa0e5c4645827bc
dragosbus <dragosbusuioc94@gmail.com>
2018-05-13 18:04:11 +0200

    appended to dom title, release date and short overview

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,11 +5,15 @@ export const Movie = props => {
     return (
         <li className="movie">
             <img src={`https://image.tmdb.org/t/p/w500${props.cover}`} alt="Cover" />
-            <div className="infos">
-                <div className="rating">
-                    <span className="score">{props.rating * 10}%</span>
-                </div>    
-                <h3 className="title">{props.title}</h3>
+            <div className="infos"> 
+                <div className="main-infos">
+                    <div className="rating">
+                        <span className="score">{props.rating * 10}%</span>
+                    </div>     
+                    <h3 className="title">{props.title}</h3>
+                    <h4 className="release">{props.date}</h4>
+                </div>
+                <p className="overview">{props.overview.slice(0,60)}...</p>
             </div>
         </li>
     );  
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,7 +6,7 @@ export class Movies extends Component {
     render() {
         return (
             <ul className="movie-list">
-                {this.props.movies.map((movie, i) => <Movie key={i} cover={movie.poster_path} title={movie.original_title} rating={movie.vote_average}/>)} 
+                {this.props.movies.map((movie, i) => <Movie key={i} cover={movie.poster_path} title={movie.original_title} rating={movie.vote_average} date={movie.release_date} overview={movie.overview}/>)} 
             </ul>
         );
     }
