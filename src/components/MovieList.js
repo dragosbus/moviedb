42e5873f76406298f4bd4daa93f7f2cf16b61e0a
dragosbus <dragosbusuioc94@gmail.com>
2018-05-13 19:06:01 +0200

    changed color border for rating depend of rating

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 
 export const Movie = props => {
+    let colorScore = props.rating * 10 >= 75 ? 'rgb(61, 199, 61)' :
+        props.rating * 10 >= 50 ? 'rgb(236, 132, 13)' : 'rgb(243, 30, 58)';    
     return (
         <li className="movie">
             <img src={`https://image.tmdb.org/t/p/w500${props.cover}`} alt="Cover" />
             <div className="infos"> 
                 <div className="main-infos">
                     <div className="rating">
-                        <span className="score">{props.rating * 10}%</span>
+                        <span style={{borderColor: colorScore}} className="score">{props.rating * 10}%</span>
                     </div>     
                     <h3 className="title">{props.title}</h3>
                     <h4 className="release">{props.date}</h4>
