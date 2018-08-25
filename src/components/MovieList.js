251fccbae7dbfe919cac23b9798014db89370656
dragosbus <dragosbusuioc94@gmail.com>
2018-08-25 12:21:27 +0200

    restyled the cover

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,10 +6,13 @@ export const Movie = props => {
     props.rating * 10 >= 75 ? 'rgb(61, 199, 61)' : props.rating * 10 >= 50 ? 'rgb(236, 132, 13)' : 'rgb(243, 30, 58)';
 
   let bgImage = `https://image.tmdb.org/t/p/w500${props.cover}`;
+  let stylesBg = {
+      backgroundImage: 'linear-gradient(180deg, rgba(2,2,2,0.55) 65%, rgba(255,255,255,0.45)), url(' + bgImage + ')',
+  }
 
   return (
     <li className="movie">
-      <div className="img" style={{ backgroundImage: `url(${bgImage})` }} />
+      <div className="img" style={stylesBg} />
       <FaStar className="favorite" />
       <div className="infos">
         <div className="main-infos">
