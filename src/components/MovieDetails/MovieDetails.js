084efb5ef4e222dda66f21d76a158890c5c9a71c
dragosbus <dragosbusuioc94@gmail.com>
2018-11-09 12:47:47 +0100

    set the runtime in minutes and hours

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -12,6 +12,14 @@ class MovieDetails extends React.Component {
       display: movie.on ? 'block' : 'none',
       backgroundImage: `linear-gradient(rgba(0,0,0,0.0) 20%, rgba(0,0,0,0.74) 45%), url(https://image.tmdb.org/t/p/w600_and_h900_bestv2${poster})`
     };
+
+    const calcRuntime = (runtime) => {
+      let runtimeInt = parseInt(runtime, 10);
+      let hour = parseInt(runtimeInt / 60, 10);
+      let minutes = runtimeInt - hour * 60;
+
+      return `${hour} h ${minutes} min`;
+    };
     
     return (
       <div className="movie-details" style={styleMovieDetails}>
@@ -22,7 +30,8 @@ class MovieDetails extends React.Component {
             <span>{movie.genres ? movie.genres[0].name : ''}</span>
           </p>
           <h4>{movie.original_title}</h4>
-          <p>{movie.runtime}</p>
+          <p>{calcRuntime(movie.runtime)}</p>
+          <p className="movie-details--overview">{movie.overview}</p>
         </div>
       </div>
     );
