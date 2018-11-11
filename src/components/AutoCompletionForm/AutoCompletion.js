3f2c12342e37248f54171fb7cac4330435734ed6
dragosbus <dragosbusuioc94@gmail.com>
2018-11-11 12:11:28 +0100

    adjust the style for the week trending component on the desktop devices

--- src/components/AutoCompletionForm/AutoCompletion.js
+++ src/components/AutoCompletionForm/AutoCompletion.js
@@ -1,30 +1,42 @@
 import React from 'react';
 import './AutoCompletion.css';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { setSearchTerm, emptyAutoCompletion} from '../../actionCreators/actionCreators';
+import { setSearchTerm, emptyAutoCompletion } from '../../actionCreators/actionCreators';
 
 const AutoCompletion = props => {
-  const {setSearchTerm, autoCompletionMovies, emptyAutoCompletion} = props;
+  const { setSearchTerm, autoCompletionMovies, emptyAutoCompletion } = props;
   return (
     <ul className="auto-completion">
       {autoCompletionMovies.slice(0, 5).map(movie => (
-        <li onClick={()=>{
-          setSearchTerm(movie.title);
-          emptyAutoCompletion();
-        }} key={movie.id}>{movie.title}</li>
+        <li
+          onClick={() => {
+            setSearchTerm(movie.title);
+            emptyAutoCompletion();
+          }}
+          key={movie.id}
+        >
+          {movie.title}
+        </li>
       ))}
     </ul>
   );
 };
 
 const mapStateToProps = state => ({
-  autoCompletionMovies: state.autoCompletion,
+  autoCompletionMovies: state.autoCompletion
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  setSearchTerm: setSearchTerm,
-  emptyAutoCompletion: emptyAutoCompletion,
-}, dispatch);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      setSearchTerm: setSearchTerm,
+      emptyAutoCompletion: emptyAutoCompletion
+    },
+    dispatch
+  );
 
-export default connect(mapStateToProps, mapDispatchToProps)(AutoCompletion);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AutoCompletion);
