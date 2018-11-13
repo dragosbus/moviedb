4d71b2df0792ef5547ee1adcbf9669ea9d5fda1a
dragosbus <dragosbusuioc94@gmail.com>
2018-11-13 17:12:42 +0100

    init filtering movies for duplicates

--- src/reducers/favorites.js
+++ src/reducers/favorites.js
@@ -1,7 +1,7 @@
 import * as ActionTypes from '../actionTypes/actionTypes';
 
-export const favoritesReducer = (state=[], action) =>{
-  switch(action.type) {
+export const favoritesReducer = (state = [], action) => {
+  switch (action.type) {
     case ActionTypes.ADD_TO_FAVORITE:
       return [...state, action.payload];
     default:
