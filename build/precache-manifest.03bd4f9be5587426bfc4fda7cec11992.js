164cd83faa647732476dc5825e513a1fc1f161be
dragosbus <dragosbusuioc94@gmail.com>
2018-11-13 12:57:42 +0100

    build

---
+++ build/precache-manifest.03bd4f9be5587426bfc4fda7cec11992.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfccda6e6e862b982c8807cb3a0861ee",
+    "url": "/moviedb/build/static/media/play-button.cfccda6e.svg"
+  },
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/moviedb/build/static/media/logo.02a9430b.svg"
+  },
+  {
+    "revision": "cdc70a0a052aaf1b30a1",
+    "url": "/moviedb/build/static/js/runtime~main.cdc70a0a.js"
+  },
+  {
+    "revision": "3773888911d7c495d742",
+    "url": "/moviedb/build/static/js/main.37738889.chunk.js"
+  },
+  {
+    "revision": "03b1f0066cf76285f0f4",
+    "url": "/moviedb/build/static/js/1.03b1f006.chunk.js"
+  },
+  {
+    "revision": "3773888911d7c495d742",
+    "url": "/moviedb/build/static/css/main.24616702.chunk.css"
+  },
+  {
+    "revision": "7f08f32a9689825059a22b6857398809",
+    "url": "/moviedb/build/index.html"
+  }
+];
