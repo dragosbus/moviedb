7dc45e227b587ae1422df9a78e1ca150a849b62f
dragosbus <dragosbusuioc94@gmail.com>
2018-11-14 13:11:22 +0100

    refactoring uniquness gandler of the favorites movies component

--- src/reducers/favorites.js
+++ src/reducers/favorites.js
@@ -4,12 +4,7 @@ export const favoritesReducer = (state = [], action) => {
   switch (action.type) {
     case ActionTypes.ADD_TO_FAVORITE:
       return [
-        ...state.map(movie => {
-          if(movie.id === action.payload.id) {
-            return null;
-          }
-          return movie;
-        }).filter(movie => movie !== null),
+        ...state.filter(movie => movie.id !== action.payload.id),
         action.payload
       ];
     default:
